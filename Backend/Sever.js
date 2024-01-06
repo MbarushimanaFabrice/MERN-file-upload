7e0def8283db6710301e356b595241b300e11968
MBARUSHIMANA Fabrice <123445030+MbarushimanaFabrice@users.noreply.github.com>
2024-01-06 11:30:48 +0200

    Create Sever.js

---
+++ Backend/Sever.js
@@ -0,0 +1,11 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const multer=require("multer");
+const app=express();
+
+app.get("",(req,res)=>{
+    
+})
+
+
+app.listen("3000")
